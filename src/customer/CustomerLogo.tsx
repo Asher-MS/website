91ded9f8562090a58c44f0d233733ff181451804
Roshan Jossy <roshan.jossy@jina.ai>
2021-06-08 12:26:39 +0200

    feat: use Nextjs template
     - Set up continuous deployment to push to GitHub pages

---
+++ src/customer/CustomerLogo.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type ICustomerLogoProps = {
+  src: string;
+  alt: string;
+};
+
+const CustomerLogo = (props: ICustomerLogoProps) => (
+  <img
+    className="opacity-10"
+    src={`${process.env.baseUrl}${props.src}`}
+    alt={props.alt}
+  />
+)
+
+export { CustomerLogo }
