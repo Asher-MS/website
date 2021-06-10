c96de811bc3dbef2a2b979fb753cbc563d0e373c
Roshanjossey <roshanjossey@gmail.com>
2021-06-10 10:15:53 +0000

    Deploying to gh-pages from  @ 2a4152c352488efc0f800388adca3eedbaabee00 🚀

--- _next/static/L8Fa-A4zBPyjVl9W3t7vz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-7354612f0e3428884069.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-136a4cec629bb4b3f633.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/lUhWjtdeWslt1y4XhHloO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/419-4b0730465d1e438bbf51.js","static/chunks/pages/index-f2cd76a6a96b24b239f8.js"],"/_error":["static/chunks/pages/_error-cbdfbe99a48321d2727c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
