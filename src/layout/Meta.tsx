8c6fcfd7c2067779dbb30e029e3afa54655520bd
Roshan Jossy <roshan.jossy@jina.ai>
2021-06-10 13:17:20 +0200

    fix favicons

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -15,24 +15,14 @@ const Meta = (props: IMetaProps) => (
   <>
     <Head>
       <meta charSet="UTF-8" key="charset" />
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1"
-        key="viewport"
-      />
+      <meta name="viewport" content="width=device-width,initial-scale=1" key="viewport" />
       <meta property="og:title" content="Jina AI" />
       <meta name="author" content="Jina AI" />
       <meta property="og:locale" content="en_US" />
       <meta property="og:site_name" content="Jina AI" />
-      <meta
-        property="og:image"
-        content="https://jina.ai/assets/images/jina_banner_new.png"
-      />
+      <meta property="og:image" content="https://jina.ai/assets/images/jina_banner_new.png" />
       <meta name="twitter:card" content="summary" />
-      <meta
-        property="twitter:image"
-        content="https://jina.ai/assets/images/jina_banner_new.png"
-      />
+      <meta property="twitter:image" content="https://jina.ai/assets/images/jina_banner_new.png" />
       <meta property="twitter:title" content="Jina AI" />
       <meta name="twitter:site" content="@JinaAI_" />
       <meta name="twitter:creator" content="@Jina AI" />
@@ -40,6 +30,13 @@ const Meta = (props: IMetaProps) => (
         rel="apple-touch-icon"
         href={`${process.env.baseUrl}/apple-touch-icon.png`}
         key="apple"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={`${process.env.baseUrl}/favicon-16x16.png`}
+        key="icon16"
       />
       <link
         rel="icon"
@@ -52,14 +49,11 @@ const Meta = (props: IMetaProps) => (
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href={`${process.env.baseUrl}/favicon-16x16.png`}
-        key="icon16"
+        href={`${process.env.baseUrl}/favicon-96x96.png`}
+        key="icon96"
       />
-      <link
-        rel="icon"
-        href={`${process.env.baseUrl}/favicon.ico`}
-        key="favicon"
-      />
+      <link rel="icon" href={`${process.env.baseUrl}/favicon.ico`} key="favicon" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
     </Head>
     <NextSeo
       title={props.title}
