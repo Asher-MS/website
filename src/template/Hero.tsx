91636f4897d60cf6269cf691198b412697556168
Roshan Jossy <roshan.jossy@jina.ai>
2021-06-21 03:34:17 +0200

    style: font size for Hero section heading

--- src/template/Hero.tsx
+++ src/template/Hero.tsx
@@ -8,10 +8,12 @@ import { Section } from '../layout/Section'
 const Hero = () => (
   <Background color="bg-white">
     <Section yPadding="pt-20 pb-3">
-      <h1 className="text-8xl font-bold text-black w-5/12">{websiteCopyStrings.heroHeadline}</h1>
+      <h1 className="text-7xl font-bold text-black w-6/12">{websiteCopyStrings.heroHeadline}</h1>
       <p className="w-4/12">{websiteCopyStrings.heroDescription}</p>
       <div className="flex mt-8 mb-40">
-        <Button className="w-56 py-5 mr-10">{websiteCopyStrings.heroPrimaryButton}</Button>
+        <Button primary className="w-56 py-5 mr-10">
+          {websiteCopyStrings.heroPrimaryButton}
+        </Button>
         <Button secondary className="w-56 py-5">
           {websiteCopyStrings.heroSecondaryButton}
         </Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       '4xl': '2.25rem',
       '5xl': '3rem',
       '6xl': '4rem',
+      '7xl': '5.3rem',
       '8xl': '6rem'
     },
     extend: {
