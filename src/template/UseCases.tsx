c6ba5626629025b7129b14560e401252b9361f3c
BastinJafari <bastinjafari@gmail.com>
2021-06-23 12:49:50 +0200

    Feat: UseCaseSet

--- src/template/UseCases.tsx
+++ src/template/UseCases.tsx
@@ -20,11 +20,12 @@ function ArrowPrev (onClickHandler: () => void, hasPrev: boolean, label: string)
               position: absolute;
               z-index: 4;
               top: calc(50% - 15px);
+              margin-left: 20px;
               width: 60px;
               height: 60px;
               cursor: pointer;
               background: #ffffff;
-              box-shadow: 8px 8px 36px -18px rgba(1, 57, 57, 0.442623);
+              box-shadow: 8px 8px 36px -18px rgba(1, 57, 57, 2);
             }
 
             .nextButton:focus {
@@ -49,12 +50,14 @@ function ArrowNext (onClickHandler: () => void, hasNext: boolean, label: string)
               position: absolute;
               z-index: 2;
               top: calc(50% - 15px);
+              margin-right: 20px;
+
               width: 60px;
               height: 60px;
               cursor: pointer;
               background: #ffffff;
               right: 0px;
-              box-shadow: 8px 8px 36px -18px rgba(1, 57, 57, 0.442623);
+              box-shadow: 8px 8px 36px -18px rgba(1, 57, 57, 2);
             }
 
             .nextButton:focus {
@@ -68,7 +71,7 @@ function ArrowNext (onClickHandler: () => void, hasNext: boolean, label: string)
   )
 }
 
-function UseCaseCard ({ src, title, link }: UseCaseCardProps) {
+function UseCaseCard ({ src, title }: UseCaseCardProps) {
   return (
     <div className="card">
       <style jsx>
@@ -106,6 +109,20 @@ function UseCaseCard ({ src, title, link }: UseCaseCardProps) {
   )
 }
 
+function UseCaseSet () {
+  return (
+    <div className="flex justify-between px-12">
+      <UseCaseCard
+        src="/assets/images/use-case-ecommerce.png"
+        title="ECOMMERCE"
+        link="www.google.de"
+      />
+      <UseCaseCard src="/assets/images/use-case-gaming.png" title="GAMING" link="www.google.de" />
+      <UseCaseCard src="/assets/images/use-case-academy.png" title="ACADEMY" link="www.google.de" />
+    </div>
+  )
+}
+
 function UseCases () {
   return (
     <Section>
@@ -115,23 +132,9 @@ function UseCases () {
         </h2>
       </div>
       <Carousel renderArrowPrev={ArrowPrev} renderArrowNext={ArrowNext}>
-        <div>
-          <UseCaseCard
-            src="/assets/images/use-case-ecommerce.png"
-            title="ECOMMERCE"
-            link="www.google.de"
-          />
-          <UseCaseCard
-            src="/assets/images/use-case-gaming.png"
-            title="GAMING"
-            link="www.google.de"
-          />
-          <UseCaseCard
-            src="/assets/images/use-case-academy.png"
-            title="ACADEMY"
-            link="www.google.de"
-          />
-        </div>
+        <UseCaseSet />
+        <UseCaseSet />
+        <UseCaseSet />
       </Carousel>
     </Section>
   )
