bbee3b30eb7f658eab47ca32697ec1714f1850b6
Roshan Jossy <roshan.jossy@jina.ai>
2021-06-25 09:30:36 +0200

    feat: add response screen for subscribe dialog

--- src/template/Subscribe.tsx
+++ src/template/Subscribe.tsx
@@ -1,53 +1,72 @@
-import React, { useState } from 'react'
+/* eslint-disable */
+import React, { useState } from 'react';
 
-import { Section } from '../layout/Section'
-import { websiteCopyStrings } from '../utils/websiteCopyStrings'
+import { Section } from '../layout/Section';
+import { websiteCopyStrings } from '../utils/websiteCopyStrings';
+import Icon from './Icon';
 
 const Subscribe = () => {
-  const [email, setEmail] = useState('')
+  const [email, setEmail] = useState('');
+  const [submitted] = useState(false);
   return (
     <div>
       <Section>
-        <div className="bg-primary-500 flex flex-col items-center max-w-screen-lg mx-auto rounded-3xl pt-16 pb-20">
-          <h2 className="font-bold text-3xl text-white">{websiteCopyStrings.subscribeTitle}</h2>
-          <p className="text-gray-100 mt-2 mb-8">{websiteCopyStrings.subscribeSubtitle}</p>
-          <form
-            action="https://jina.us17.list-manage.com/subscribe/post?u=135040dff373660039d95c86c&amp;id=6873344704"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            className="validate w-full"
-            target="_blank"
-          >
-            <div id="mc_embed_signup_scroll" className="flex flex-row w-full justify-center">
-              {/* <!-- <label for="mce-EMAIL">Subscribe</label> --> */}
-              <input
-                className="rounded-lg p-2 w-3/6"
-                type="email"
-                value={email}
-                placeholder={websiteCopyStrings.subscribeInputPlaceholder}
-                name="EMAIL"
-                id="mce-EMAIL"
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-              {/* <!-- <div aria-hidden="true"><input type="text" name="b_135040dff373660039d95c86c_6873344704" tabindex="-1" value=""></div> --> */}
-              <div className="clear">
-                <input
-                  className="text-black bg-secondary-500 rounded-lg ml-4 p-2"
-                  type="submit"
-                  value={websiteCopyStrings.subscribeButtonLabel}
-                  name="subscribe"
-                  id="mc-embedded-subscribe"
-                />
+        <div className="bg-primary-500 flex flex-col items-center max-w-screen-lg mx-auto rounded-3xl pt-16 pb-16">
+          {submitted ? (
+            <>
+              <div className="flex">
+                <Icon name="paper-plane" className="mr-10" />
+                <div className="">
+                  <h2 className="font-bold text-3xl text-white">
+                    {websiteCopyStrings.subscribeTitle}
+                  </h2>
+                  <p className="text-gray-100 mt-2">{websiteCopyStrings.subscribeSubtitle}</p>
+                </div>
               </div>
-            </div>
-          </form>
+            </>
+          ) : (
+            <>
+              <h2 className="font-bold text-3xl text-white">{websiteCopyStrings.subscribeTitle}</h2>
+              <p className="text-gray-100 mt-2 mb-8">{websiteCopyStrings.subscribeSubtitle}</p>
+              <form
+                action="https://jina.us17.list-manage.com/subscribe/post?u=135040dff373660039d95c86c&amp;id=6873344704"
+                method="post"
+                id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form"
+                className="validate w-full"
+                target="_blank"
+              >
+                <div id="mc_embed_signup_scroll" className="flex flex-row w-full justify-center">
+                  {/* <!-- <label for="mce-EMAIL">Subscribe</label> --> */}
+                  <input
+                    className="rounded-lg p-2 w-3/6"
+                    type="email"
+                    value={email}
+                    placeholder={websiteCopyStrings.subscribeInputPlaceholder}
+                    name="EMAIL"
+                    id="mce-EMAIL"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                  {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+                  {/* <!-- <div aria-hidden="true"><input type="text" name="b_135040dff373660039d95c86c_6873344704" tabindex="-1" value=""></div> --> */}
+                  <div className="clear">
+                    <input
+                      className="text-black bg-secondary-500 rounded-lg ml-4 p-2"
+                      type="submit"
+                      value={websiteCopyStrings.subscribeButtonLabel}
+                      name="subscribe"
+                      id="mc-embedded-subscribe"
+                    />
+                  </div>
+                </div>
+              </form>
+            </>
+          )}
         </div>
       </Section>
     </div>
-  )
-}
+  );
+};
 
-export default Subscribe
+export default Subscribe;
