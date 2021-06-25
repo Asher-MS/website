f8629598e6371bc47183b6d42206ef0ae203ec9c
Roshan Jossy <roshan.jossy@jina.ai>
2021-06-25 03:07:43 +0200

    refactor: planets
    Currently using one base planet and extending other planets for it

--- src/hero/Planets.tsx
+++ src/hero/Planets.tsx
@@ -2,60 +2,56 @@ import React from 'react'
 import { DivProps } from '../button/Button'
 import Icon from '../template/Icon'
 
-type RotateStyleProps = {
-  planetSuffix: string;
-  duration: number;
-};
-
 interface PlanetProps extends DivProps {
   rotationPeriod: number;
+  name?: string;
 }
 
-const RotateStyles = ({ planetSuffix, duration }: RotateStyleProps) => (
-  <style jsx>
-    {`
-      @keyframes revolute {
-        from {
-          transform: rotate(360deg);
+export const Planet = ({ className, rotationPeriod, name }: PlanetProps) => (
+  <div className={`${className} planet-${name} bg-yellow-500 rounded-full p-4 w-16 h-16`}>
+    <Icon name={`${name}-white`} className="" />
+    <style jsx>
+      {`
+        @keyframes revolute {
+          from {
+            transform: rotate(360deg);
+          }
+          to {
+            transform: rotate(0deg);
+          }
         }
-        to {
-          transform: rotate(0deg);
+        .planet-${name} {
+          animation-iteration-count: infinite;
+          animation-timing-function: linear;
+          animation-name: revolute;
+          animation-duration: ${rotationPeriod}s;
         }
-      }
-      .planet-${planetSuffix} {
-        animation-iteration-count: infinite;
-        animation-timing-function: linear;
-        animation-name: revolute;
-        animation-duration: ${duration}s;
-      }
-    `}
-  </style>
+      `}
+    </style>
+  </div>
 )
 
 export const PlanetHub = ({ className, rotationPeriod }: PlanetProps) => (
-  <div className={`${className} planet-hub bg-yellow-500 rounded-full p-4 w-16 h-16`}>
-    <Icon name="hub-white" className="" />
-    <RotateStyles planetSuffix="hub" duration={rotationPeriod} />
-  </div>
+  <Planet
+    name="hub"
+    className={`${className} bg-yellow-500`}
+    rotationPeriod={rotationPeriod}
+  ></Planet>
 )
 export const PlanetCore = ({ className, children, rotationPeriod }: PlanetProps) => (
-  <div className={`${className} planet-core bg-red-500 rounded-full p-4 w-16 h-16`}>
-    <Icon name="core-white" />
+  <Planet name="core" className={`${className} bg-red-500`} rotationPeriod={rotationPeriod}>
     {children}
-    <RotateStyles planetSuffix="core" duration={rotationPeriod} />
-  </div>
+  </Planet>
 )
 export const PlanetCode = ({ className, rotationPeriod }: PlanetProps) => (
-  <div className={`${className} planet-code bg-blue-500 rounded-full p-4 w-16 h-16`}>
-    <Icon name="code-white" className="rotate-90" />
-    <RotateStyles planetSuffix="code" duration={rotationPeriod} />
-  </div>
+  <Planet name="code" className={`${className} bg-blue-500`} rotationPeriod={rotationPeriod} />
 )
 export const PlanetDashboard = ({ className, rotationPeriod }: PlanetProps) => (
-  <div className={`${className} planet-dashboard bg-primary-500 rounded-full p-4 w-16 h-16`}>
-    <Icon name="dashboard-white" />
-    <RotateStyles planetSuffix="dashboard" duration={rotationPeriod} />
-  </div>
+  <Planet
+    name="dashboard"
+    className={`${className} bg-primary-500`}
+    rotationPeriod={rotationPeriod}
+  />
 )
 
 export const PlanetWithMoon = () => (
@@ -64,7 +60,6 @@ export const PlanetWithMoon = () => (
       <div className="absolute top-1/2 left-1/2">
         <div className="planet-moon-axis relative">
           <div className="absolute bg-primary-400 w-4 h-4 -top-12 -left-12 rounded-full"></div>
-          <RotateStyles planetSuffix="moon-axis" duration={2} />
         </div>
       </div>
     </PlanetCore>
