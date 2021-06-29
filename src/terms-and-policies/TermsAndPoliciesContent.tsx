52bb02e390fc44ef252dfd2a68e7e0f98ae30ea5
Roshan Jossy <roshan.jossy@jina.ai>
2021-06-29 11:19:59 +0200

    add content for terms and policies

---
+++ src/terms-and-policies/TermsAndPoliciesContent.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { websiteCopyStrings } from '../utils/websiteCopyStrings'
+
+const TermsAndPoliciesContent = () => (
+  <div className="w-10/12">
+    <ul>
+      <li id="subtitle-1">
+        <h4>{websiteCopyStrings.termsAndPoliciesSubTitle1}</h4>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription1Part1}</p>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription1Part2}</p>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription1Part3}</p>
+      </li>
+      <li id="subtitle-2">
+        <h4>{websiteCopyStrings.termsAndPoliciesSubTitle2}</h4>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription2}</p>
+      </li>
+      <li id="subtitle-3">
+        <h4>{websiteCopyStrings.termsAndPoliciesSubTitle3}</h4>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription3}</p>
+      </li>
+      <li id="subtitle-4">
+        <h4>{websiteCopyStrings.termsAndPoliciesSubTitle4}</h4>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription4}</p>
+      </li>
+      <li id="subtitle-5">
+        <h4>{websiteCopyStrings.termsAndPoliciesSubTitle5}</h4>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription5}</p>
+      </li>
+      <li id="subtitle-6">
+        <h4>{websiteCopyStrings.termsAndPoliciesSubTitle6}</h4>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription6}</p>
+      </li>
+      <li id="subtitle-7">
+        <h4>{websiteCopyStrings.termsAndPoliciesSubTitle7}</h4>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription7}</p>
+      </li>
+      <li id="subtitle-8">
+        <h4>{websiteCopyStrings.termsAndPoliciesSubTitle8}</h4>
+        <p>{websiteCopyStrings.termsAndPoliciesDescription8}</p>
+      </li>
+    </ul>
+  </div>
+)
+
+export default TermsAndPoliciesContent
---
+++ src/terms-and-policies/TermsAndPoliciesSidebar.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { websiteCopyStrings } from '../utils/websiteCopyStrings'
+
+const TermsAndPoliciesSidebar = () => (
+  <div className="w-2/12">
+    <ul className="flex flex-col">
+      <li className="mr-6">
+        <a className="" href="#subtitle-1">
+          {websiteCopyStrings.termsAndPoliciesSubTitle1}
+        </a>
+      </li>
+      <li className="mr-6">
+        <a className="" href="#subtitle-2">
+          {websiteCopyStrings.termsAndPoliciesSubTitle2}
+        </a>
+      </li>
+      <li className="mr-6">
+        <a className="" href="#subtitle-3">
+          {websiteCopyStrings.termsAndPoliciesSubTitle3}
+        </a>
+      </li>
+      <li className="mr-6">
+        <a className="" href="#subtitle-4">
+          {websiteCopyStrings.termsAndPoliciesSubTitle4}
+        </a>
+      </li>
+      <li className="mr-6">
+        <a className="" href="#subtitle-5">
+          {websiteCopyStrings.termsAndPoliciesSubTitle5}
+        </a>
+      </li>
+      <li className="mr-6">
+        <a className="" href="#subtitle-6">
+          {websiteCopyStrings.termsAndPoliciesSubTitle6}
+        </a>
+      </li>
+      <li className="mr-6">
+        <a className="" href="#subtitle-7">
+          {websiteCopyStrings.termsAndPoliciesSubTitle7}
+        </a>
+      </li>
+      <li className="mr-6">
+        <a className="" href="#subtitle-8">
+          {websiteCopyStrings.termsAndPoliciesSubTitle8}
+        </a>
+      </li>
+      <li className="mr-6">
+        <a className="" href="#subtitle-9">
+          {websiteCopyStrings.termsAndPoliciesSubTitle9}
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default TermsAndPoliciesSidebar
