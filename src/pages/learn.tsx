ab3ac893b8e039be74aeed8cd3d5c644a62d6a91
Roshan Jossy <roshan.jossy@jina.ai>
2021-07-07 13:15:57 +0200

    Remove hero section for learn page

--- src/pages/learn.tsx
+++ src/pages/learn.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import HeroSectionLearnPage from '../hero/HeroSectionLearnPage'
 
 import Meta from '../layout/Meta'
 import PageWithHeaderAndFooter from '../layout/PageWithHeaderAndFooter'
@@ -11,9 +10,7 @@ const Learn = () => (
       description="Open source neural search ecosystem for businesses and developers, allowing anyone to search any kind of data with high availability and scalability."
       canonical="https://jina.ai/"
     />
-    <PageWithHeaderAndFooter>
-      <HeroSectionLearnPage />
-    </PageWithHeaderAndFooter>
+    <PageWithHeaderAndFooter></PageWithHeaderAndFooter>
   </>
 )
 
