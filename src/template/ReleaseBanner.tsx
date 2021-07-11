be3c775467e9b24ac5cf1782fa3738b2cf176d22
Roshan Jossy <roshan.jossy@jina.ai>
2021-07-11 02:36:59 +0200

    style: release banner

--- src/template/ReleaseBanner.tsx
+++ src/template/ReleaseBanner.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import { Button } from '../button/Button'
-import { websiteCopyStrings } from '../utils/websiteCopyStrings'
+import { GITHUB_LINK, websiteCopyStrings } from '../utils/websiteCopyStrings'
 
 const ReleaseBanner = () => {
   const [showBanner, setShowBanner] = useState(true)
@@ -9,20 +9,27 @@ const ReleaseBanner = () => {
   return showBanner
     ? (
     <div className="release-banner">
-      <span>{websiteCopyStrings.heroReleaseBannerText}</span>
-      <Button className="inline-block">{websiteCopyStrings.heroReleaseBannerButtonLabel}</Button>
-      <div className="inline-block text-right" onClick={() => setShowBanner(false)}>
-        <svg height={10} width={10}>
-          <line x1={0} y1={0} x2={10} y2={10} style={{ stroke: '#000', strokeWidth: 1 }} />
-          <line x1={0} y1={10} x2={10} y2={0} style={{ stroke: '#000', strokeWidth: 1 }} />
+      <span className="font-medium text-black ml-auto">
+        {websiteCopyStrings.heroReleaseBannerText}
+      </span>
+      <a href={GITHUB_LINK}>
+        <Button secondary className="inline-block px-10 py-1.5 ml-6">
+          {websiteCopyStrings.heroReleaseBannerButtonLabel}
+        </Button>
+      </a>
+      <div className="inline-block ml-auto mr-6" onClick={() => setShowBanner(false)}>
+        <svg height={16} width={16}>
+          <line x1={0} y1={0} x2={16} y2={16} style={{ stroke: '#000', strokeWidth: 1 }} />
+          <line x1={0} y1={16} x2={16} y2={0} style={{ stroke: '#000', strokeWidth: 1 }} />
         </svg>
       </div>
       <style jsx>
         {`
           .release-banner {
-            @apply w-full absolute top-24;
+            @apply w-full py-5 absolute;
             @apply bg-secondary-300;
-            @apply text-center;
+            @apply flex justify-center items-center;
+            top: 5.5rem;
             z-index: 120;
           }
         `}
