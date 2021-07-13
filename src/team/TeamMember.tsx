6ab20b90acd9a7b703ff45dd721b100db7799fe7
Sergiy Chemodanov <sergiychemodanov@gmail.com>
2021-07-13 16:15:05 -0500

    fix: rm unnecessary div

--- src/team/TeamMember.tsx
+++ src/team/TeamMember.tsx
@@ -11,12 +11,10 @@ export const TeamMember = ({ teamMember }: Props) => {
   const size = teamMember.size || DEFAULT_SIZE
   const { avatarUrl, coords } = teamMember
   return (
-    <div>
-      <img
-        src={avatarUrl}
-        className="absolute rounded-full shadow-lg cursor-pointer transition duration-200 hover:transform hover:scale-150 hover:shadow-xl hover:z-10"
-        style={{ width: `${size}%`, left: `${coords[0]}%`, top: `${coords[1]}%` }}
-      />
-    </div>
+    <img
+      src={avatarUrl}
+      className="absolute rounded-full shadow-lg cursor-pointer transition duration-200 hover:transform hover:scale-150 hover:shadow-xl hover:z-10"
+      style={{ width: `${size}%`, left: `${coords[0]}%`, top: `${coords[1]}%` }}
+    />
   )
 }
